a3f3fd4a8c2173d60c85f0e953b586d5f93cfcaf
Idanis Gallimore <IGallimore@aco.local>
2025-01-28 09:53:15 -0500

    Day one project done

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,7 @@
-# Write your code below this line 👇
+print ("Hello, \n Welcome to the band name generator!!! \n Feel free to use this tool to come up with a new name for your awersome band!!!")
+print ("Let's get started.")
+city = input("What is the name of the city that you were born in?")
+pet = input("What is the name of your pet?")
+
+print("Congrats! \n Here is your new band name")
+print ("---> " + city  + " " + pet)
