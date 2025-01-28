9bbd15acd206929f1a0de15b66094459b8347a5a
Idanis Gallimore <IGallimore@aco.local>
2025-01-28 11:31:34 -0500

    day 2 done

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,27 @@
 bmi = 84 / 1.65 ** 2
+
+#floor -> removes the numbers after decimal and rounds down to the lowest number
+
+print (int(33.56677777))
+
+print (round(33.567777898))
+
+print (round(108.564444, 1))
+#round gives you the option to choose the number of digits after decimal (if you want decimal)
+
+number_one = 108
+number_two = 33
+number_3 = 44
+
+number_one += 100
+print(number_one)
+
+print(f"my favorite number is: {number_two}")
+#f strings are awesome!
+
+
+
+
+
+
+
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -1,6 +1,32 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
+#total of bill
+#percentage of tip 10, 15 or 20
+#how many people to split the bill with
+
+print("Welcome to the tip calculator! Let's get started!")
+
+total_bill = input ("What is the total of the bill? ")
+tip_percent = input ("How much do you want to tip? 10, 15 or 20 percent? ")
+total_people = input ("How many people will be splitting this bill? ")
+
+tip_percent = int(tip_percent)
+total_bill = float(total_bill)
+print(total_bill)
+total_bill = round(total_bill, 2)
+print(total_bill)
+total_people = int(total_people)
+the_tip = tip_percent / 100
+print(the_tip)
+the_complete_tip = total_bill * the_tip
+the_total = total_bill + round(the_complete_tip, 2)
+
+print(f"Here is the total: ${the_total}")
+print (f"Everyone gets to pay: ${round(the_total / total_people, 2)}")
+
+
+
+
+
+
+
 
 
