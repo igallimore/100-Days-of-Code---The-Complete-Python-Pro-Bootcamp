40ae634450b5ad36ed1de38c9659b49000963339
Idanis Gallimore <Idanis1908@gmail.com>
2025-02-04 15:06:03 -0500

    started the hangman. Stuck, so I am stepping away from it to recoup.

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,46 @@
-word_list = ["aardvark", "baboon", "camel"]
+import random
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+word_list = ["candy", "car", "mother", "pasta", "doggie", "perspective"]
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+chosen_word = random.choice(word_list)
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+counter = len(chosen_word)
+
+print(chosen_word)
+print (counter)
+
+guess = input ("Pick a letter").lower()
+print(guess)
+
+placeholder = []
+
+for x in range(counter):
+	placeholder.append("_")
+
+places = " ".join(placeholder)
+
+print(places)
+
+display = []
+
+
+
+# for letter in chosen_word:
+# 	if guess == letter:
+# 		print ("Right!")
+# 		display.append(letter)
+# 	else:
+# 		print ("Wrong!")
+# 		display.append("_")
+# display = " ".join(display)
+# print (display)
+
+
+
+while counter > 0:
+	for letter in chosen_word:
+		if guess != letter:
+			counter -= 1
+	if counter == 0:
+		print ("You lost!")
+	guess = input ("pick a letter")
