19dc4615352a2aaded93f155766f86d11805334c
Idanis Gallimore <Idanis1908@gmail.com>
2025-02-07 11:31:18 -0500

    Started day 8. Finally!

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,12 @@
+
+# user_input = input ("What is your name?")
+def greet(name):
+	print (f"Hello {name}!\n How are you today?")
+
+# greet(user_input)
+
+names = ["Idanis", "Brad Pitt", "Kantufla", "Candy Jane", "Lolli Anne", "Daniel"]
+
+for name in names:
+	greet(name)
+
