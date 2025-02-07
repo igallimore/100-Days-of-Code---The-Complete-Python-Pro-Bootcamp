e1de2fbaefd5727d0e60f38d60f9a32518d35981
Idanis Gallimore <Idanis1908@gmail.com>
2025-02-07 10:15:08 -0500

    finishing the hangman

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,46 +1,109 @@
 import random
-
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["candy", "car", "mother", "pasta", "doggie", "perspective"]
 
 chosen_word = random.choice(word_list)
 
-counter = len(chosen_word)
+counter =  len(chosen_word)
 
 print(chosen_word)
 print (counter)
 
-guess = input ("Pick a letter").lower()
-print(guess)
-
-placeholder = []
+placeholder = ""
 
 for x in range(counter):
-	placeholder.append("_")
+	placeholder += "_"
+print(placeholder)
 
-places = " ".join(placeholder)
+game_over = False
+correct_letters = []
 
-print(places)
+lives = 6
 
-display = []
+while not game_over:
+    display = " "
+    guess = input("Pick a letter: ")
+
+    # Test
+    if guess not in chosen_word:
+        print(stages[lives])
+        lives -= 1
+        # print("This is the lives guess")
+
+    for letter in chosen_word:
+        if lives == 0:
+            print ("You lost!")
+            print (stages[0])
+            exit()
+        if guess == letter:
+            # print("Right!")
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            # print("Wrong!")
+            display += "_"
+
+    if "_" not in display:
+        game_over = True
+        print ("You win!")
+    print(display)
 
 
 
-# for letter in chosen_word:
-# 	if guess == letter:
-# 		print ("Right!")
-# 		display.append(letter)
-# 	else:
-# 		print ("Wrong!")
-# 		display.append("_")
-# display = " ".join(display)
-# print (display)
-
-
-
-while counter > 0:
-	for letter in chosen_word:
-		if guess != letter:
-			counter -= 1
-	if counter == 0:
-		print ("You lost!")
-	guess = input ("pick a letter")
